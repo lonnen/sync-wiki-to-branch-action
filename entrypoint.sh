b26fa932a841ca6abddaeb8810b1e1678d48be6b
lonnen <chris.lonnen@gmail.com>
2023-04-09 16:07:06 -0700

    set exit immediately flag (-e)
    anything unexpected happening should cause this to fail fast, so we can remove the small amount of error handling previously installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
+set -e
 
 GITHUB_FULL_REPO_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
 
 git clone "${GITHUB_FULL_REPO_URL}".wiki.git wiki
-cd wiki || exit
+cd wiki
 git remote add main "${GITHUB_FULL_REPO_URL}".git
 git push main master:"${TARGET_BRANCH}"
