e928d6db301af5842b21f4e2ba32871b2da278d2
lonnen <chris.lonnen@gmail.com>
2023-04-10 00:22:25 -0700

    provide the token when interacting with github

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 GITHUB_FULL_REPO_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
+GITHUB_TOKEN_AND_REPO_URL="${TOKEN}@${GITHUB_FULL_REPO_URL}"
 
-git clone "${GITHUB_FULL_REPO_URL}".wiki.git wiki
+git clone "${GITHUB_TOKEN_AND_REPO_URL}".wiki.git wiki
 cd wiki
-git remote add main "${GITHUB_FULL_REPO_URL}".git
+git remote add main "${GITHUB_TOKEN_AND_REPO_URL}".git
 git push main master:"${TARGET_BRANCH}"
