2cb44490bdcc75bb2401cfc12ff9bf0004e4586f
lonnen <chris.lonnen@gmail.com>
2024-01-21 21:52:49 -0800

    error if token is unset or empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -l
 set -e
 
+if [ -z "$GITHUB_TOKEN" ]; then
+    # shellcheck disable=SC2016
+    echo '$GITHUB_TOKEN is unset or empty, check your actions configuration'
+    exit 1
+fi
+
 GITHUB_TOKEN_AND_REPO_URL="https://github-actions:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
 git clone "${GITHUB_TOKEN_AND_REPO_URL}".wiki.git wiki
