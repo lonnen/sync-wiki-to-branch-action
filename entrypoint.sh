26fd24508e0203b49856ea0e04f06d66460ad7e1
lonnen <chris.lonnen@gmail.com>
2024-01-21 17:14:42 -0800

    change remote name to repo
    this removes some of the ambiguity of the prior remote name "main", which is also the default branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ GITHUB_TOKEN_AND_REPO_URL="https://github-actions:${GITHUB_TOKEN}@github.com/${G
 
 git clone "${GITHUB_TOKEN_AND_REPO_URL}".wiki.git wiki
 cd wiki
-git remote add main "${GITHUB_TOKEN_AND_REPO_URL}".git
-git push main master:"${TARGET_BRANCH}"
+git remote add repo "${GITHUB_TOKEN_AND_REPO_URL}".git
+git push repo master:"${TARGET_BRANCH}"
