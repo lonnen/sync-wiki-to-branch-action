1bd65617997274cf087539cef6f388f91de2e094
lonnen <chris.lonnen@gmail.com>
2024-01-21 22:08:35 -0800

    print a more helpful error message if wiki checkout fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -l
-set -e
 
 if [ -z "$GITHUB_TOKEN" ]; then
     # shellcheck disable=SC2016
@@ -7,9 +6,17 @@ if [ -z "$GITHUB_TOKEN" ]; then
     exit 1
 fi
 
+# GITHUB_REPOSITORY is automatically injected by actions and does not need a check
+
 GITHUB_TOKEN_AND_REPO_URL="https://github-actions:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
 git clone "${GITHUB_TOKEN_AND_REPO_URL}".wiki.git wiki
+if [ $? -ne 0 ]; then
+    echo "Failed to check out ${GITHUB_TOKEN_AND_REPO_URL}.wiki.git"
+    echo "please check your actions configuration"
+    exit 1
+fi
+
 cd wiki
 git remote add repo "${GITHUB_TOKEN_AND_REPO_URL}".git
 git push repo master:"${TARGET_BRANCH}"
